424d45e8e0e744debe581b164a716b020f51dd50
minkolee <minkolee@gmail.com>
2021-11-28 22:35:56 +0800

    会使用图标了

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import AccountBalanceWalletRoundedIcon from '@mui/icons-material/AccountBalanceWalletRounded';
+import AccountBalanceWalletTwoToneIcon from '@mui/icons-material/AccountBalanceWalletTwoTone';
+import AccountBalanceWalletSharpIcon from '@mui/icons-material/AccountBalanceWalletSharp';
+
+const styleBottom = {"verticalAlign": "bottom"};
 
 function App() {
   return (
     <div>
-    Get Ready!
-    </div>
+      <ul>
+        <li >Filled: <AccountBalanceWalletIcon  color="primary" fontSize="small" style={styleBottom}/></li>
+        <li>Outlined: <AccountBalanceWalletOutlinedIcon color="secondary" style={styleBottom}/></li>
+        <li>Rounded: <AccountBalanceWalletRoundedIcon color="success" fontSize="large" style={styleBottom}/></li>
+        <li>TwoTone: <AccountBalanceWalletTwoToneIcon color="action" style={styleBottom}/></li>
+        <li>Sharp: <AccountBalanceWalletSharpIcon color="disabled" sx={{fontSize:40}} style={styleBottom}/></li>
+      </ul>
+   </div>
   );
 }
 
