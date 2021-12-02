efc7f733be70df22b1253492bb9e3e9f5ec244e6
minkolee <minkolee@gmail.com>
2021-12-02 16:54:08 +0800

    修改Link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import {Container, CssBaseline} from "@mui/material";
 import React from "react";
 import LoginPage from "./component/LoginPage";
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Container, CssBaseline, Link} from "@mui/material";
 
 class App extends React.Component {
 
     render() {
-
         return (
             <Router>
                 <Route exact path="/">
+                    <Container maxWidth="lg">
+                    <CssBaseline>
                     <div>
                         <h2>Main Page</h2>
-                        <Link to="/login">Login</Link>
+                        <Link href="/login" color="primary" sx={{textDecoration:"none"}}>登录</Link>
                     </div>
+                    </CssBaseline>
+                    </Container>
                 </Route>
                 <Route exact path="/login">
                     <LoginPage/>
