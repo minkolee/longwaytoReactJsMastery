6c8e3a397c8749f62fba95436cac1aa1f543fc5b
minkolee <minkolee@gmail.com>
2021-12-04 15:07:53 +0800

    会使用图标了

---
+++ src/component/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Container, CssBaseline, Link} from "@mui/material";
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+Hello world!
+            </div>
+        )
+    }
+}
+
+export default App;
